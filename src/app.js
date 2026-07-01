0f8e711966e8a75e56ed7bd32f6c78e2a540312b
shivanshu <sr4858244@gmail.com>
2026-07-01 18:22:14 +0530

    add file system

--- src/app.js
+++ src/app.js
@@ -1,11 +1,77 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const path = require("path");
 
+const port = 8080;
 
+app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Root Route");
+});
 
+app.post("/submission", (req, res) => {
 
+    // Validation
+    if (!req.body.title) {
+        return res.json({ success: false, message: "Title is required" });
+    }
 
-app.listen(8080,(req,res)=>{
-    console.log("port is listing ");
+    if (!req.body.language) {
+        return res.json({ success: false, message: "Language is required" });
+    }
+
+    if (!req.body.code) {
+        return res.json({ success: false, message: "Code is required" });
+    }
+
+    if (!req.body.difficulty) {
+        return res.json({ success: false, message: "Difficulty is required" });
+    }
+
+    const { title, language, difficulty, code } = req.body;
+
+    // Create folders so ".." is tells that one folder up and create 
+    const solutionPath = path.join(__dirname,".." , "solutions");
+    const languagePath = path.join(solutionPath, language);
+    const difficultyPath = path.join(languagePath, difficulty);
+
+    if (!fs.existsSync(solutionPath)) {
+        fs.mkdirSync(solutionPath);
+    }
+
+    if (!fs.existsSync(languagePath)) {
+        fs.mkdirSync(languagePath);
+    }
+
+    if (!fs.existsSync(difficultyPath)) {
+        fs.mkdirSync(difficultyPath);
+    }
+
+    // Decide file extension
+    let extension = ".txt";
+
+    if (language === "Java") extension = ".java";
+    else if (language === "Python") extension = ".py";
+    else if (language === "C++") extension = ".cpp";
+    else if (language === "JavaScript") extension = ".js";
+
+    // Create file
+    const filePath = path.join(
+        difficultyPath,
+        `${title}${extension}`
+    );
+
+    // Write code
+    fs.writeFileSync(filePath, code);
+
+    return res.json({
+        success: true,
+        message: "Submission saved successfully."
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
